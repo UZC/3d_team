489e2d95c8d5dfaff7682ed583bdd71eedf66858
Bayson4 <57019426+Bayson4@users.noreply.github.com>
2019-11-04 23:59:31 +0200

    added movement script
    move+rotation

---
+++ 3D Team/Assets/Scripts/vdmytrenkoFolder/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public float speed;
+    public float rotationSpeed;
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        
+    }
+    public void Move(Vector3 direction)
+    {
+        rigidBody.velocity = direction * speed;
+    }
+
+    public void Rotate(Vector3 rotation)
+    {
+        this.transform.Rotate(rotation * rotationSpeed);
+    }
+}
