1dabbd024c2df20083ce9105ad192a4b86498181
Bayson4 <vladickdmitrenko@gmail.com>
2019-11-17 23:08:16 +0200

    first build

---
+++ 3D Team/Assets/Scripts/vdmytrenkoFolder/HealthBars.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBars : MonoBehaviour
+{
+    [SerializeField]
+    Image player1;
+    [SerializeField]
+    Image player2;
+
+    [SerializeField]
+    Health healthOfPlayer1;
+    [SerializeField]
+    Health healthOfPlayer2;
+    void Start()
+    {
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        player1.fillAmount = (float)healthOfPlayer1.GetHealth() / 100;
+        player2.fillAmount = (float)healthOfPlayer2.GetHealth() / 100;
+    }
+}
