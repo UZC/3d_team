e47d846f205c5118c078a179b05a2a3134d30af4
azlok21 <56533909+azlok21@users.noreply.github.com>
2019-11-25 22:34:42 +0200

    Update skill cooldown

---
+++ 3D Team/Assets/Scripts/SkillCooldown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCooldown : MonoBehaviour
+{
+    float cooldown;
+    float currentCooldown;
+    public Image cooldownIcon;
+    public Text cooldownText;
+    bool flagStart;
+    void Update()
+    {
+        if (flagStart)
+        {
+            if (currentCooldown >= cooldown)
+            {
+                cooldownIcon.fillAmount = 0;
+                cooldownText.text = cooldown.ToString();
+                flagStart = false;
+            }
+            if (currentCooldown < cooldown)
+            {
+                currentCooldown += Time.deltaTime;
+                cooldownIcon.fillAmount = currentCooldown / cooldown;
+                cooldownText.text = (cooldown - currentCooldown).ToString();
+
+            }
+        }
+    }
+    public void StartCooldown(float cooldown)
+    {
+        cooldown = this.cooldown;
+        flagStart = true;
+    }
+}
+
+
+
