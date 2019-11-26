aaa63b2dc7851fdb6435e8cf33e63759dcb07683
Artemis <lonedriver9x@gmail.com>
2019-11-26 22:06:30 +0200

    Added VictoryScreen canvas and Pause script

---
+++ 3D Team/Assets/Scripts/apustovyi/Pause.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject victoryScreen;
+
+    void Update()
+    {
+        stopAndReload();
+    }
+
+    public void stopAndReload()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Time.timeScale == 1)
+            {
+                victoryScreen.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                victoryScreen.SetActive(false);
+                Time.timeScale = 1;
+            }
+
+        }
+        
+    }
+    public void RestartGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Time.timeScale = 1;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
